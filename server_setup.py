9d315cd0f1ef28d575e629ee376cd3d35ea33662
OP Raaja <gc5674656@gmail.com>
2025-03-18 08:57:38 +0530

    Added a new minecraft version

--- server_setup.py
+++ server_setup.py
@@ -14,7 +14,8 @@ def choice():
     versions={"1.21.4":"https://piston-data.mojang.com/v1/objects/4707d00eb834b446575d89a61a11b5d548d8c001/server.jar",
               "1.21.3":"https://piston-data.mojang.com/v1/objects/45810d238246d90e811d896f87b14695b7fb6839/server.jar",
               "1.21.2":"https://piston-data.mojang.com/v1/objects/7bf95409b0d9b5388bfea3704ec92012d273c14c/server.jar",
-              "1.21.1":"https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar"}
+              "1.21.1":"https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar",
+              "1.21":"https://piston-data.mojang.com/v1/objects/450698d1863ab5180c25d7c804ef0fe6369dd1ba/server.jar"}
 
     print(Fore.YELLOW + "Choose from the list of available versions given below: ")
     for key in versions.keys():
